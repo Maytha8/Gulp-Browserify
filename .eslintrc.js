3f3ab79bea32724f92f73373a7d0a3a61ac58b09
Maytham <44939173+Maytha8@users.noreply.github.com>
2022-08-25 18:36:49 +0800

    Configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es2021': true,
+        'node': true,
+    },
+    'extends': 'google',
+    'overrides': [
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+    },
+    'rules': {
+        'require-jsdoc': 'off',
+        'indent': ['error', 4],
+        'max-len': 'off',
+    },
+};
